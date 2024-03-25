190788012cb0f3c526096039a775242ec3fa50e5
VenelinDavidov <114108014+VenelinDavidov@users.noreply.github.com>
2024-03-25 22:09:04 +0200

    error

--- ExamPrepare2024/ExamPrepare2021/01. High Quality Structure_Skeleton/restaurant/src/main/java/restaurant/entities/healthyFoods/interfaces/HealthyFood.java
+++
@@ -1,9 +0,0 @@
-package restaurant.entities.healthyFoods.interfaces;
-
-public interface HealthyFood {
-    String getName(); // име
-
-    double getPortion();  // порция
-
-    double getPrice();    // цена
-}
